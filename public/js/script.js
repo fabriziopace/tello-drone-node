675847f1c739a749c6280fc78cafa1cc7aad3cc6
Fabrizio Pace <pacefabriziowork@gmail.com>
2019-10-21 18:00:50 +0200

    Camera streaming

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
-const socket = io();
+async function init() {
+    startStreaming();
+}
 
-socket.on('battery', (battery) => {
-    $('.progress-bar')[0].style.width = `${battery}%`;
-    $('.progress-bar').text(`Tello Battery ${battery}%`);
-});
+const startStreaming = async function() {
+    await fetch('/streamon', { method: 'POST' })
+
+    const tellocamera = document.getElementById('tellocamera');
+    const winLoc = window.location;
+    const webSockUrl = `ws://${winLoc.hostname}:${winLoc.port}/`;
+
+    const telloPlayer = new JSMpeg.Player(webSockUrl, {
+        canvas: tellocamera,
+        audio: false,
+        videoBufferSize: 512 * 1024,
+        preserveDrawingBuffer: true
+    });
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    setTimeout(init, 500)
+};
--- src/index.js
+++ src/index.js
@@ -1,87 +1,81 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
-const dgram = require('dgram');
-const http = require('http');
-const socketio = require('socket.io');
-const server = http.Server(app);
-const io = socketio(server);
+const Tello = require('./tello');
+const { spawn } = require('child_process');
+const ws = require('ws');
 
-const port = '8889'; // Tello UDP port
-const host = '192.168.10.1'; // Tello ip
-const portState = '8890'; // Tello state UDP port
-const portVideo = '11111'; // Tello video stream UDP port
-const hostVideo = '0.0.0.0'; // Tello video stream UDP ip
-const interfacePort = '3000'; // Front-end interface port
+const telloHost = '192.168.10.1';
+const telloPortCamera = 11111;
+const localPort = 3000;
+const localHost = 'localhost';
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-// Create sockets for Tello commands and Tello video streaming
-const tello = dgram.createSocket('udp4');
-const telloState = dgram.createSocket('udp4');
-const telloCamera = dgram.createSocket('udp4');
+// Create new instance of tello class
+const tello = new Tello();
 
-// Tello socket error messages handler
-const errorFromTello = (errorMessage) => {
-    return errorMessage ? console.log(`Error ${errorMessage}`) : '';
-};
-
-// In order to send command and receive response
-tello.bind(port);
-tello.on('message', message => {
-    // console.log(`Tello says: ${message}`);
-    io.sockets.emit('battery', parseInt(message));
-});
-
-// In order to receive Tello state
-telloState.bind(portState);
-telloState.on('message', function(state) {
-    console.log(state);
-});
-
-// Before sending any commands is required to send the following command 
-tello.send('command', 0, 'command'.length, port, host, errorFromTello);
-
-// Tello starts streaming
-tello.send('streamon', 0, 'streamon'.length, port, host, errorFromTello);
-
-telloCamera.on('listening', function() {
-    var address = telloCamera.address();
-    console.log(`UDP server listening on ${address.address} : ${address.port}`);
-});
-
-
-// tello.send('takeoff', 0, 'command'.length, port, host, errorFromTello);
-
-// setTimeout(function() {
-//     tello.send('land', 0, 'command'.length, port, host, errorFromTello);
-// }, 3000);
-
-telloCamera.bind(portVideo, hostVideo);
-
-// Interface Front-End
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
+// Open Tello camera and start streaming
+app.post(`/streamon`, async(req, res) => {
+    await tello.sendCommand('command');
+    await tello.sendCommand('streamon');
+    res.end()
+});
+
+// Close Tello camera
+app.post('/streamoff', async(req, res) => {
+    await tello.sendCommand('command');
+    await tello.sendCommand('streamoff');
+});
+
+// Ws Tello streaming
+app.post(`/streaming`, (req, res) => {
+    res.connection.setTimeout(0);
+    req.on('data', function(data) {
+        webSocket.broadcast(data)
+    });
+});
+
+// Interface
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-io.on('connection', (socket) => {
-    socket.on('command', command => {
-        tello.send(command, 0, command.length, port, host, errorFromTello);
+// Express server
+const server = app.listen(localPort, localHost);
 
-        // socket.emit, io.emit, socket.broadcast.emit
-        // io.to.emit, socket.broadcast.to.emit
-    });
+// WebSocket Server for stream Tello Camera
+const webSocket = new ws.Server({ server });
 
-    // Check Tello Battery Each 5 Seconds
-    setInterval(function() {
-        tello.send('battery?', 0, 'battery?'.length, port, host, errorFromTello);
-    }, 5000);
-});
+// WebSocket Server Broadcast, when the video data is sent to client 
+webSocket.broadcast = function(data) {
+    webSocket.clients.forEach(function each(client) {
+        if (client.readyState === ws.OPEN) {
+            client.send(data);
+        }
+    })
+};
 
-server.listen(interfacePort, () => {
-    console.log(`Server is up on port ${interfacePort}`)
-});
+// FFMEG - use udp for stream video
+const ffmpeg = spawn('ffmpeg', [
+    '-hide_banner',
+    '-i',
+    `udp://${telloHost}:${telloPortCamera}`,
+    '-f',
+    'mpegts',
+    '-codec:v',
+    'mpeg1video',
+    '-s',
+    '640x480',
+    '-b:v',
+    '800k',
+    '-bf',
+    '0',
+    '-r',
+    '20',
+    `http://${localHost}:${localPort}/streaming`
+]);
